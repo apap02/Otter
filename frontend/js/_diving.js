e0d81ea51401c081e57366473d56f5407756ec75
apap02 <apap02@gmail.com>
2015-08-04 14:53:06 +0800

    前端監控動作
    定期向後端上傳記錄的資料

---
+++ frontend/js/_diving.js
@@ -0,0 +1,52 @@
+(function(window, undefined){
+	var dive = dive || {
+		options: {
+			server: '',
+			api: 'api/OtterDiving/syncRecords',	
+		},
+		vardosync: undefined,	//timeout varable
+		records: [],
+		addRecord: function(userID, targetID, actDesc, actData){
+			var tmp,
+				dtime = new Date();
+			tmp.uid = userID;
+			tmp.tid = targetID;
+			tmp.desc = actDesc;
+			tmp.data = actData;
+			tmp.dtime = dtime.getFullYear() + '-' + (dtime.getMonth() + 1) + '-' + dtime.getDate() + ' ' + dtime.getHours() + ':' + dtime.getMinutes() + ':' + dtime.getSeconds();
+			//tmp.dtime = dtime.toJSON();	//IE9
+			records.push(tmp);
+		},
+		//---- set Timeout ------------------------------------------------------------------------
+		syncRecords:function(){
+			if(Dive.vardosync)
+				clearTimeout(Dive.vardosync);
+			Dive.vardosync = setTimeout(Dive.doSyncRecords,	1500);
+		},
+		//---- ¤W¶Ç¦Üserver ------------------------------------------------------------------------
+		doSyncRecords: function(){
+			if(Dive.records.length == 0){
+				Dive.syncRecords();
+				return;
+			}
+			
+			$.ajax({
+				method: 'POST',
+                url: Dive.options.server + Dive.options.api,
+				data: { 'records' : Dive.records},
+                beforeSend: function (xhr) {
+                    Comm.setHeader(xhr);
+				}
+			}).done(function (data) {
+                if(Comm.isSuccess(data)){
+					Dive.records = [];					
+				}
+				Dive.syncRecords();
+            });
+		}
+	};
+	
+	window.Dive = dive;
+})(window);
+
+Dive.syncRecords();
